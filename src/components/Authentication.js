2114518a8604c9a2816f041dada5e8a433d24013
cmlee <dlcndaks12@naver.com>
2017-06-21 13:14:25 +0900

    scss, fonts 세팅완료. *todo : 라우터 세팅 필요.

---
+++ src/components/Authentication.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Authentication extends Component {
+  render() {
+    return (
+      <div>
+        Auth
+      </div>
+    );
+  }
+}
+
+Authentication.propTypes = {};
+Authentication.defaultProps = {};
+
+export default Authentication;
---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/style.scss';
+import { Header } from '../components';
+
+class App extends Component {
+  render() {
+    return (
+      <Header/>
+
+    );
+  }
+}
+
+export default App;
