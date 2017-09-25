4370a38d0721309e6f643011e3cf4d8050cf88d2
CUL-CMLEE-NOTE\admin <dlcndaks12@naver.com>
2017-09-25 14:04:57 +0900

    path changed

--- src/pages/index.async.js
+++ src/pages/index.async.js
@@ -1,7 +1,7 @@
 import asyncRoute from 'lib/asyncRoute';
 
-export const Home = asyncRoute(() => import('./Home'));
-export const Login = asyncRoute(() => import('./Login'));
-export const Register = asyncRoute(() => import('./Register'));
-export const Write = asyncRoute(() => import('./Write'));
-export const NoMatch = asyncRoute(() => import('./NoMatch'));
+export const Home = asyncRoute(() => import('.//Home/Home'));
+export const Login = asyncRoute(() => import('./Auth/Login'));
+export const Register = asyncRoute(() => import('./Auth/Register'));
+export const Write = asyncRoute(() => import('./Article/Write'));
+export const NoMatch = asyncRoute(() => import('./Common/NoMatch'));
