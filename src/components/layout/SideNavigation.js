cd8d3412a10f15bd6623850a849aec1842193685
CUL-CMLEE-NOTE\admin <dlcndaks12@naver.com>
2017-10-19 17:47:03 +0900

    side navigation 수정

--- src/components/layout/SideNavigation.js
+++ src/components/layout/SideNavigation.js
@@ -5,24 +5,32 @@ import { SideNav } from 'react-materialize';
 
 class SideNavigation extends Component {
     render() {
-        const id = this.props.status.id !== '' ? this.props.status.id : '안녕하세요.';
+        // const id = this.props.status.id !== '' ? this.props.status.id : '안녕하세요.';
 
         return (
             <SideNav
                 trigger={<a><i className="material-icons">menu</i></a>}
                 options={{ closeOnClick: true }}>
+                {this.props.status.isLoggedIn ?
+                  <li className="my-area blue lighten-2">
+                    {/*<Link to="/mypage" className="name">{this.props.status.id}</Link>*/}
 
-                <div className="my-area blue lighten-2">
-                    <Link to="/mypage" className="name">{id}</Link>
+                    <Link to="/mypage" className="name">Admin</Link>
                     <ul className="info-list">
-                      <li title="내가 쓴 글 갯수">
-                        <i className="tiny white-text material-icons">border_color</i><span>10</span>
+                      <li>
+                        <span>3241</span>개의 스크랩을 하셨어요!
+                        {/*<i className="tiny white-text material-icons">border_color</i><span>10</span>*/}
                       </li>
-                      <li title="내가 쓴 댓글 갯수">
-                        <i className="tiny white-text material-icons">mode_comment</i><span>0</span>
+                      <li>
+                        <span>3241</span>개의 리뷰를 작성하셨어요!
+                        {/*<i className="tiny white-text material-icons">mode_comment</i><span>0</span>*/}
                       </li>
                     </ul>
-                </div>
+                  </li> :
+                  <li className="welcome-area">
+                    <Link to="/login" className="blue lighten-2">로그인 해보세요.</Link>
+                  </li>
+                }
                 <li>
                   <NavLink to="/scrap" activeClassName="active">
                     <i className="small material-icons">folder_special</i><span>스크랩</span>
